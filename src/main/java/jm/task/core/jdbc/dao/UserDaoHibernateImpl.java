9e34b4b78d46319abafce19183a198ae5cc92d57
Kirill Zagurny <perfecto.grosso@gmail.com>
2024-02-15 15:54:56 +0300

    configure hibernate

--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,7 +1,11 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
 
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
@@ -9,10 +13,24 @@ public class UserDaoHibernateImpl implements UserDao {
 
     }
 
-
     @Override
     public void createUsersTable() {
-
+//        String query = "CREATE TABLE USERS (" +
+//                "ID BIGINT(19) NOT NULL AUTO_INCREMENT UNIQUE," +
+//                "NAME VARCHAR(64) NOT NULL," +
+//                "LAST_NAME VARCHAR(64) NOT NULL," +
+//                "AGE TINYINT NOT NULL," +
+//                "CONSTRAINT PRIMARY KEY (ID)" +
+//                ");";
+//
+//        try (PreparedStatement preparedStatement = Util.getConnection().prepareStatement(query);) {
+//            if (!isTableExists("USERS")) {
+//                preparedStatement.execute();
+//                System.out.println("Таблица USERS создана");
+//            } else {
+//                System.out.println("Таблица USERS уже существует");
+//            }
+//        }
     }
 
     @Override
@@ -39,4 +57,30 @@ public class UserDaoHibernateImpl implements UserDao {
     public void cleanUsersTable() {
 
     }
+
+//    public boolean isTableExists(String tableName) {
+//        String query = "SHOW TABLES;";
+//        try (PreparedStatement preparedStatement = Util.getConnection().prepareStatement(query)) {
+//            ResultSet resultSet = preparedStatement.executeQuery();
+//            while (resultSet.next()) {
+//                if (resultSet.getString(1).equals(tableName)) {
+//                    return true;
+//                }
+//            }
+//            return false;
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+//
+//    public boolean isUserExists(long id) {
+//        String query = "SELECT * FROM USERS WHERE ID = ?;";
+//        try (PreparedStatement preparedStatement = Util.getConnection().prepareStatement(query)) {
+//            preparedStatement.setLong(1, id);
+//            ResultSet resultSet = preparedStatement.executeQuery();
+//            return resultSet.next();
+//        } catch (SQLException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
 }
--- src/main/java/jm/task/core/jdbc/model/User.java
+++ src/main/java/jm/task/core/jdbc/model/User.java
@@ -1,21 +1,21 @@
 package jm.task.core.jdbc.model;
 
-import javax.persistence.Column;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 
-@Table
+@Table(name="USERS")
 public class User {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="ID")
     private Long id;
 
-    @Column
+    @Column(name="NAME")
     private String name;
 
-    @Column
+    @Column(name="LAST_NAME")
     private String lastName;
 
-    @Column
+    @Column(name="AGE")
     private Byte age;
 
     public User() {
